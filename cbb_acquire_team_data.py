b3fe6779d1782730b16c1459f5c3aa0100e29fbd
Artem Yankov <yankovai@umich.edu>
2013-11-15 11:04:54 -0500

    Added restart capability to cbb_acquire_team_data.py

--- cbb_acquire_team_data.py
+++ cbb_acquire_team_data.py
@@ -14,12 +14,13 @@ class CBB_Acquire_Team_Data(object):
         self.year2 = year2
         self.get_team_names(year1, year2)
         self.csv_filename = csv_filename
-        self.progress_id = 0
         self.total_work = (year2 - year1 + 1)*len(self.team_names)
 
-    def __call__(self):
+    def __call__(self, start_id):
         """
-        Creates the csv output file for further analysis. 
+        Creates the csv output file for further analysis. The variable 
+        'start_id' specifies which data entry out of 'total_work' to start 
+        recording in the output csv file. 
         """
 
         with open(self.csv_filename, 'wb') as csvfile:
@@ -29,21 +30,25 @@ class CBB_Acquire_Team_Data(object):
                                      'DRB','TRB','AST','STL','BLK','TOV','PF',\
                                      'PTS','PTSg'])
 
+            self.progress_id = 1
             for year in range(self.year1, self.year2 + 1):
                 for team_name in self.team_names:
-                    team_stats = [team_name, year]
-                    team_stats += self.get_team_stats(team_name, year)
-                    self.csvwriter.writerow(team_stats)
-                    self._print_progress()
-                    time.sleep(1)
+                    if self.progress_id >= start_id: 
+                        team_stats = [team_name, year]
+                        team_stats += self.get_team_stats(team_name, year)
+                        self.csvwriter.writerow(team_stats)
+                        self._print_progress()
+                        time.sleep(1)
+                    else:
+                        self.progress_id += 1
 
     def _print_progress(self):
         """
         Prints fraction of total work completed.
         """
         
-        self.progress_id += 1
         print 'Completed %d of %d' %(self.progress_id, self.total_work)
+        self.progress_id += 1
         
     def get_team_stats(self, team_name, year):
         """
@@ -73,7 +78,6 @@ class CBB_Acquire_Team_Data(object):
                     team_stats.append(float(result.string))
                 else:
                     team_stats.append(None)
-        
         else:
             team_stats += [None]*21
 
@@ -88,14 +92,14 @@ class CBB_Acquire_Team_Data(object):
         base_url = 'http://www.sports-reference.com/cbb/schools/'
         response = urllib2.urlopen(base_url)
         content = response.read()
-        soup = BeautifulSoup(content)
+        soup = BeautifulSoup(content, 'html.parser')
         soup_results = soup.findAll('tr', {'class':''})
         extract_name = lambda name: name.split('/')[3]
         team_names = []
         
         for result in soup_results[1::]:
-            year_span = result.findAll('td', {'align':'center'})       
-            year_span = map(int, [year.text for year in year_span])
+            year_span = result.findAll('td', {'align':'center'})      
+            year_span = map(int, [year.string for year in year_span])
 
             if year_span[0] <= year1 and year_span[1] >= year2:
                 team_name = result.find('a', href = True).get('href')
@@ -105,9 +109,8 @@ class CBB_Acquire_Team_Data(object):
         self.team_names = team_names
 
 if __name__ == "__main__":
-    # Finished 638
     cbb = CBB_Acquire_Team_Data(2003, 2013, 'cbb_team_data.csv')
-    cbb()
+    cbb(639)
 
 
 
