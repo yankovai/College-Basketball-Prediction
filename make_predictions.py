fb7c0302fde5a13bf19378dff9ba4ef1c5e28b59
yankovai <yankovai@umich.edu>
2013-12-07 15:22:00 -0500

    Finished make_predictions class

--- make_predictions.py
+++ make_predictions.py
@@ -4,6 +4,7 @@ from sklearn import linear_model
 from sklearn import preprocessing
 import sqlite3 as lite
 import cPickle
+import csv
 
 def acquire_current_season_data(current_season = 2014):
     """
@@ -33,6 +34,36 @@ def acquire_current_season_data(current_season = 2014):
     pml = Prepare_for_ML(score_data_filename, db_filename, ratings_dict_filename)
     pml.process_raw_data(team_data_filename)
 
+def make_tableau_file(mp, current_season = 2014):
+    """
+    Produces a csv file containing predicted and actual game results for the 
+    current season. Tableau uses the contents of the file to produce
+    visualizations.
+    """
+    
+    score_data_filename = 'score_data' + str(current_season) + '.csv'
+    
+    with open('tableau_input.csv','wb') as writefile:
+        tableau_write = csv.writer(writefile)
+        tableau_write.writerow(['Team 1', 'Opponent','Team 1 Points', 'Opponent Points', 'True Result', 'Predicted Result', 'Confidence'])
+        
+        with open(score_data_filename,'rb') as readfile:
+            scores = csv.reader(readfile)
+            scores.next()
+            
+            for score in scores:
+                tableau_content = score[1::]
+                # Append 'True Result'
+                if int(tableau_content[2]) > int(tableau_content[3]):
+                    tableau_content.append(1.)
+                else:
+                    tableau_content.append(0.)
+                # Append 'Predicted Result' and 'Confidence'
+                prediction_results = mp.make_prediction(tableau_content[0], tableau_content[1])
+                tableau_content += list(prediction_results)
+                
+                tableau_write.writerow(tableau_content)
+                    
 class Make_Predictions(object):
     """
     Applies machine learning techniques to the scraped colleged basketball data
@@ -84,7 +115,7 @@ class Make_Predictions(object):
         prediction_output = self.logreg.predict(feature) 
         prediction_probability = max(self.logreg.predict_proba(feature)[0])
         
-        return prediction_output, prediction_probability
+        return prediction_output[0], prediction_probability
         
     def scale_features(self, X):
         """
@@ -98,11 +129,50 @@ class Make_Predictions(object):
         
         scores = cv.cross_val_score(self.logreg, self.X, self.y, cv = 10)
         print scores.mean(), scores.std()
+    
+    def rank_teams(self):
+        """
+        Use the predictive model to simulate how each team would fare against
+        all the other teams. Rank the teams based on their winning percentage.
+        """
+        
+        rd = self.ratings_dict
+        team_names = rd[rd.keys()[0]].keys()
+        nteams = len(team_names)
+        team_win_percentages = np.zeros(nteams)
+        
+        for team, i in zip(team_names, range(nteams)):
+            nwins = 0.
+            for opponent in team_names:
+                nwins += self.make_prediction(team, opponent)[0]
+            
+            team_win_percentages[i] = (nwins - 1.)/(nteams - 1.)
+        
+        # Output rankings to a csv file        
+        rank_indices = np.argsort(-team_win_percentages)
+        with open('rankings.csv','wb') as csvfile:
+            rankings_file = csv.writer(csvfile)
+            rankings_file.writerow(['Team 1', 'Win Percentage'])
+            for rank_index in rank_indices:
+                rankings_file.writerow([team_names[rank_index], team_win_percentages[rank_index]])
+                
+                
+if __name__ == '__main__':
+    # Uncomment to acquire data to make predictions for the current season.
+#    acquire_current_season_data(current_season = 2014) 
 
+    # Uncomment to train logistic regression model using archived data. Can make
+    # predictions using Make_Predictions class.
+    mp = Make_Predictions('features_archive.npz', 'ratings_dict2014.cpickle', 'cbb_data_2014.db')
+    mp.train_logistic_regression()
+    
+    # Uncomment to rank the current season's teams. Need to have 
+    # Make_Predictions class initiated. 
+#    mp.rank_teams()
+    
+    # Uncomment to create data files for tableau visualization. Need to have
+    # Make_Predictions class initiated.
+    make_tableau_file(mp)
 
-mp = Make_Predictions('features_archive.npz', 'ratings_dict2014.cpickle', 'cbb_data_2014.db')
-mp.train_logistic_regression()
-
-print mp.make_prediction('indiana', 'michigan')
 
 
